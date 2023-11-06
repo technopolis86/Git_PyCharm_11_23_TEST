6102b1fcf560ef332079ff77b7893b6dbbe1fc29
Max <dimakss@mail.ru>
2023-11-06 18:22:17 +0500

    My_4_commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     print('My first git program')
     print('And I change it every day')
     print('TESTTESTTESTTESTTEST')
+    print(11111111111111111111111111111111111111)
 
 
 if __name__ == '__main__':
